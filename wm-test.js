d9a66b0370c09825aa0444f89b5c375c077e5612
Simon Metson <simonmetson@googlemail.com>
2020-12-27 22:12:26 +0000

    change the wm tests to a layout

--- wm-test.js
+++ wm-test.js
@@ -1,8 +1,17 @@
 console.log(document.monetization);
 
-function logMessage(message) {
+function logMessage(message, event) {
+    console.log(message);
     let li = document.createElement("li");
     li.textContent = message;
+
+    if (event) {
+        let pre = document.createElement("pre");
+        let code = document.createElement("code");
+        code.textContent = JSON.stringify(event.detail, null, 2);
+        pre.appendChild(code);
+        li.appendChild(pre);
+    }
     document.getElementById("info").appendChild(li);
 }
 
@@ -12,32 +21,36 @@ if (document.monetization) {
     logMessage('Browser does not support WebMonetization');
 }
 
-let progress_done = 0;
-let pending_done = 0;
-
+var progress_done = 0;
+var pending_done = 0;
 
 function startEventHandler(event) {
-    console.log(event);
-    let message = "WM started: " + JSON.stringify(event.detail);
-    logMessage(message);
+    let message = "WM started: ";
+    logMessage(message, event);
 }
 
 function pendingEventHandler(event) {
-    console.log(event);
     pending_done += 1;
-    let message = "WM pending: " + JSON.stringify(event.detail);
-    logMessage(message);
+    let message = "WM pending: ";
+    logMessage(message, event);
     if (pending_done > 4) {
         document.monetization.removeEventListener('monetizationprogress', progressEventHandler);
     }
 }
 
 function progressEventHandler(event) {
-    console.log(event);
     progress_done += 1;
-    let message = "WM progress: " + done + " " + JSON.stringify(event.detail);
-    logMessage(message);
-    if (progress_done > 4) {
+
+    let message = "WM progress: " + progress_done + " ";
+    if (progress_done < 6) {
+        logMessage(message, event);
+    }
+    if (progress_done < 100) {
+        if (progress_done % 10 === 0) {
+            logMessage(message, event);
+        }
+    }
+    if (progress_done === 100) {
         document.monetization.removeEventListener('monetizationprogress', progressEventHandler);
     }
 }
